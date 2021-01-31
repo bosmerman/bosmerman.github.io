94a5888e5d2cf8f99617f455458cc757ad24f7db
bosmerman <78296519+bosmerman@users.noreply.github.com>
2021-01-31 04:28:56 -0600

    Add files via upload
    added more files

---
+++ Perestroika_Intermission.js
@@ -0,0 +1,19 @@
+var desireHours = 18;
+var desireMinutes = 58;
+var desireSeconds = 0;
+
+var d = new Date();
+var nowHours = d.getHours();
+var nowMinutes = d.getMinutes();
+var nowSeconds = d.getSeconds();
+
+var diffHoursinSec = (desireHours - nowHours)*3600;
+var diffMinutesinSec = (desireMinutes - nowMinutes)*60;
+var diffSecondsinSec = (desireSeconds - nowSeconds);
+
+offsetTiminginMillisec = (diffHoursinSec + diffMinutesinSec + diffSecondsinSec)*1000;
+
+setTimeout(myFunctionREDIRECT, offsetTiminginMillisec);
+function myFunctionREDIRECT() {
+  location.replace("VSM_pp2.html")
+}
